6a96b63c24493cc7d30ca5afd491fcecbb7de49c
muvvyynn <144988391+muvvyynn@users.noreply.github.com>
2025-06-26 12:04:59 +0700

    Update filesystem.c

--- src/filesystem.c
+++ src/filesystem.c
@@ -1,6 +1,7 @@
 #include "kernel.h"
 #include "std_lib.h"
 #include "filesystem.h"
+#include "std_type.h"
 
 void fsInit() {
   struct map_fs map_fs_buf;
@@ -22,6 +23,9 @@ void fsRead(struct file_metadata* metadata, enum fs_return* status) {
    * ...
    */
 
+   bool found = false;
+   int index = -1;
+
   readSector(&data_fs_buf, FS_DATA_SECTOR_NUMBER);
   readSector(&(node_fs_buf.nodes[0]), FS_NODE_SECTOR_NUMBER);
   readSector(&(node_fs_buf.nodes[32]), FS_NODE_SECTOR_NUMBER);
@@ -30,6 +34,28 @@ void fsRead(struct file_metadata* metadata, enum fs_return* status) {
    *  add your code here
    * ...
    */
+  // cara tau direktori gmn? oh == 0xff
+
+  for(int i = 0; i < 64; i++)
+  { 
+    if(!strcmp(node_fs_buf.nodes[i].node_name, metadata->node_name) && 
+      metadata->parent_index == node_fs_buf.nodes[i].parent_index) {found = true; index = i; break;} 
+  }
+
+  if(!found) {*status = FS_R_NODE_NOT_FOUND; return;}
+  if(node_fs_buf.nodes[index].data_index == FS_NODE_D_DIR) {*status = FS_R_TYPE_IS_DIRECTORY; return;}
+  
+  metadata->filesize=0;
+  int index_data = node_fs_buf.nodes[index].data_index;
+  for (int i = 0; i < FS_MAX_SECTOR; i++)
+  {
+    if(data_fs_buf.datas[index_data].sectors[i] == 0x00) break;
+
+    readSector(metadata->buffer + i * SECTOR_SIZE, data_fs_buf.datas[index_data].sectors[i]);
+    metadata->filesize += SECTOR_SIZE;
+  }
+
+  *status = FS_SUCCESS;
 }
 
 // TODO: 3. Implement fsWrite function
@@ -37,4 +63,7 @@ void fsWrite(struct file_metadata* metadata, enum fs_return* status) {
   struct map_fs map_fs_buf;
   struct node_fs node_fs_buf;
   struct data_fs data_fs_buf;
+
+  
+
 }
